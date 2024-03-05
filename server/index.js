9748c28eef112c8a1f5856bdc35ca82082aa259d
auschanh <auschanh@gmail.com>
2024-03-05 18:21:47 -0500

    forgot server folder

---
+++ server/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const cors = require('cors');
+const { Server } = require('socket.io');
+
+app.use(cors());
+const server = http.createServer(app);
+
+server.listen(3001, () => {
+    console.log("SERVER RUNNING");
+}); 
+
+const io = new Server(server, {
+    cors: {
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST"],
+    }
+});
+
+io.on("connection", (socket) => { // every connection has a unique socket id
+    console.log(`User Connecetd: ${socket.id}`); // prints socket id of connection
+
+    socket.on("disconnect", () => {
+        console.log("User Disconnected: ", socket.id);
+    });
+});
+
+
+
