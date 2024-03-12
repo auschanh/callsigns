87809066875758e71cffa90cc65a830efd5ccad9
auschanh <auschanh@gmail.com>
2024-03-12 18:31:47 -0400

    setting up form

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,37 +1,49 @@
-import React from 'react';
-import { Link, useLocation } from 'react-router-dom';
-import styles from '../css/tailwindStylesLiterals';
-import Button from '../components/Button';
+import React from "react";
+import { Link, useLocation } from "react-router-dom";
+import styles from "../css/tailwindStylesLiterals";
+// import Button from "../components/Button";
+import { Button } from "../components/ui/button";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "../components/ui/dialog";
+// import { ProfileForm } from "../components/FormPlay";
+import { Form } from "../components/ui/form";
 
-const Home = function(props) {
+const Home = function (props) {
+  const location = useLocation();
 
-    const location = useLocation();
+  return (
+    <div className="h-screen w-screen bg-blue-950 flex flex-col items-center">
+      <h1 className="text-white font-semibold text-7xl p-8 text-center mt-72">
+        Just One
+      </h1>
 
-    return (
+      <div className="flex flex-row w-[35vw] justify-evenly">
+        <Dialog>
+          <DialogTrigger>
+            <Button className={styles.greenButton}>Open</Button>
+          </DialogTrigger>
+          <DialogContent>
+            <DialogHeader>
+              <DialogTitle>Create A Room</DialogTitle>
+              <DialogDescription>
+                <Form></Form>
+              </DialogDescription>
+            </DialogHeader>
+          </DialogContent>
+        </Dialog>
 
-        <div className="h-screen w-screen bg-blue-950 flex flex-col items-center">
-
-            <h1 className="text-white font-semibold text-7xl p-8 text-center mt-72">Just One</h1>
-
-            <div className="flex flex-row w-[35vw] justify-evenly">
-
-                <Link to="game">
-
-                    <Button btnClass={styles.greenButton} btnID="play-btn" label="Play" />
-
-                </Link>
-
-                <Link to="howToPlay" state={ { previousLocation: location } } >
-
-                    <Button btnClass={styles.redButton} btnID="htp-btn" label="How To Play" />
-
-                </Link>
-
-            </div>
-
-        </div>
-        
-    );
-}
+        <Link to="howToPlay" state={{ previousLocation: location }}>
+          <Button className={styles.redButton}>How To Play</Button>
+        </Link>
+      </div>
+    </div>
+  );
+};
 
 export default Home;
