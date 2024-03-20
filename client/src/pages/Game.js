96bede6fd59bc542cdf53231d62f0a3b207834f2
auschanh <auschanh@gmail.com>
2024-03-20 19:15:02 -0400

    simple sm scale word generator button

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,12 +1,37 @@
-import React from 'react';
+import { React, useState } from "react";
+import { Button } from "../components/ui/button";
 
-const Game = function(props) {
+const Game = function (props) {
+  const [word, setWord] = useState("");
+  const words = [
+    "Coffee",
+    "Americano",
+    "Latte",
+    "Flat White",
+    "Espresso",
+    "Mocha",
+  ];
+  const generateWord = () => {
+    const generatedWord = words[Math.floor(Math.random() * words.length)];
+    setWord(generatedWord);
+  };
 
-    return (
-
-        <h1>Game</h1>
-        
-    );
-}
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <div>
+        <h1 className="font-bold text-2xl text-center">Game</h1>
+      </div>
+      <div className="w-40 text-center">
+        <Button
+          className="border-2 border-blue-600 text-white"
+          onClick={generateWord}
+        >
+          Generate a Word
+        </Button>
+      </div>
+      {word && <div className="mt-4">Generated Word: {word}</div>}
+    </div>
+  );
+};
 
 export default Game;
