c325710903c9b934ec2f9920db78f5489e751954
Justin Wong <justin.c.wong1@gmail.com>
2024-03-22 15:02:49 -0400

    set up function to display status of back end and set up naming convention for rooms

--- server/index.js
+++ server/index.js
@@ -25,8 +25,6 @@ app.use((err, req, res, next) => {
     res.status(500).send('Something broke!');
 });
 
-let roomLookup = [];
-
 io.on("connection", (socket) => { // every connection has a unique socket id
 
     console.log(`User Connected: ${socket.id}`); // prints socket id of connection
@@ -45,7 +43,6 @@ io.on("connection", (socket) => { // every connection has a unique socket id
             };
 
             usernames.push(temp);
-
     
         });
 
@@ -73,57 +70,16 @@ io.on("connection", (socket) => { // every connection has a unique socket id
 
     }
 
-    // // list of all rooms (room name => people in this room)
-    // console.log(io.sockets.adapter.rooms);
-
-    // // list of all people (person => rooms they are in)
-    // console.log(io.sockets.adapter.sids);
-
-    // // array of rooms
-    // console.log([...io.sockets.adapter.rooms.keys()]);
-
-    // array of people
-    // console.log([...io.sockets.adapter.sids.keys()]);
-
-    // // full socket information
-    // console.log(io.sockets.sockets.get(socket.id));
-
     getSocketInfo();
     
 
     socket.on("gameInfo", ({ username, roomName, numPlayers, aiPlayers }) => {
 
-        console.log(username, roomName, numPlayers, aiPlayers);
-
         socket.username = username;
 
-        socket.join(`room-${roomLookup.length}`);
+        socket.join(`${roomName}-${socket.id}`);
 
-        // either works to send a message to the room
-        // io.sockets.in(`room-${roomLookup.length}`).emit('connectToRoom', "You are in room no. " + roomLookup.length);
-        io.to(`room-${roomLookup.length}`).emit('connectToRoom', "You are in room no. " + roomLookup.length);
-
-        roomLookup.push({
-
-            roomID: `room-${roomLookup.length}`,
-            roomName: roomName,
-    
-        });
-
-        // console.log(roomLookup);
-
-        console.log(`username set: ${socket.username}`);
-
-        // const socketIDsInRoom = io.sockets.adapter.rooms.get(`room-${roomLookup.length - 1}`);
-
-        // which socketIDs are in this room?
-        // console.log(socketIDsInRoom);
-
-        // how many socketIDs are in this room?
-        // console.log(io.sockets.adapter.rooms.get(`room-${roomLookup.length - 1}`).size);
-
-        // which rooms is this socketID in?
-        console.log(socket.rooms);
+        getSocketInfo();
 
     });
 
