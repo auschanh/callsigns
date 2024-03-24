51d3fe796b93552d51ac84f0424e8ac26df36020
Justin Wong <justin.c.wong1@gmail.com>
2024-03-24 02:12:11 -0400

    added functionality to automatically include the minimum number of ai players given the player count

--- client/src/components/CreateGameForm.js
+++ client/src/components/CreateGameForm.js
@@ -111,6 +111,14 @@ export default function CreateGameForm({ setGameInfo, nextSlide }) {
 
 		form.setValue("aiPlayers", 0);
 
+		if (players < 3) {
+
+			setIsAiPlayers(true);
+
+			form.setValue("aiPlayers", 3 - players);
+
+		}
+
 	}
 
 	return (
@@ -219,6 +227,12 @@ export default function CreateGameForm({ setGameInfo, nextSlide }) {
 										className="data-[state=checked]:bg-slate-600 data-[state=unchecked]:bg-slate-400"
 										checked={isAiPlayers}
 										onCheckedChange={() => {
+
+											if (playerCount < 3) {
+
+												return;
+
+											}
 											
 											setIsAiPlayers(!isAiPlayers); 
 											
@@ -244,42 +258,76 @@ export default function CreateGameForm({ setGameInfo, nextSlide }) {
 
 											if (index + 1 <= (7 - playerCount)) {
 
-												return (
+												if (index + 1 === 1 && playerCount === 1) {
 
-													<FormItem
-														key={index + 1}
-														className="flex items-center"
-													>
-														<FormLabel
-															className={`
-																cursor-pointer h-10 px-3 py-2 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300
-																border border-slate-200 bg-white text-slate-500 hover:bg-slate-900/80 hover:text-slate-50 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50
-																duration-300
-																${field.value === index + 1
-																	? "border bg-slate-900 text-slate-50 outline ring-offset-white ring-2 ring-slate-950 ring-offset-2"
-																	: ""
-																}
-															`}
+													return (
+
+														<FormItem
+															key={index + 1}
+															className="flex items-center"
 														>
-															<FormControl>
-																<RadioGroupItem
-																	value={index + 1}
-																	className="invisble h-0 w-0 border-none"
-																	onClick={() => {
-																		
-																		if (form.getValues("aiPlayers") === 0) {
+															<FormLabel
+																className={`
+																	h-10 px-3 py-2 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300
+																	border border-slate-200 bg-slate-500/50 text-slate-50/30 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50
+																`}
+															>
+																<FormControl>
+																	<RadioGroupItem
+																		value={index + 1}
+																		className="invisble h-0 w-0 border-none"
+																		disabled
+																	/>
+																</FormControl>
+																{index + 1}
+															</FormLabel>
+														</FormItem>
 
-																			form.setValue("aiPlayers", index + 1);
-												
-																		}
-																	}}
-																/>
-															</FormControl>
-															{index + 1}
-														</FormLabel>
-													</FormItem>
+													);
+
+												} else {
+
+													return (
+
+														<FormItem
+															key={index + 1}
+															className="flex items-center"
+														>
+															<FormLabel
+																className={`
+																	cursor-pointer h-10 px-3 py-2 inline-flex items-center justify-center whitespace-nowrap rounded-md text-sm font-medium transition-colors disabled:pointer-events-none disabled:opacity-50 dark:ring-offset-slate-950 dark:focus-visible:ring-slate-300
+																	border border-slate-200 bg-white text-slate-500 hover:bg-slate-900/80 hover:text-slate-50 dark:border-slate-800 dark:bg-slate-950 dark:hover:bg-slate-800 dark:hover:text-slate-50
+																	duration-300
+																	${field.value === index + 1
+																		? "border bg-slate-900 text-slate-50 outline ring-offset-white ring-2 ring-slate-950 ring-offset-2"
+																		: ""
+																	}
+																`}
+															>
+																<FormControl>
+																	<RadioGroupItem
+																		value={index + 1}
+																		className="invisble h-0 w-0 border-none"
+																		onClick={() => {
+																			
+																			if (form.getValues("aiPlayers") === 0 
+																					|| (playerCount === 1 && form.getValues("aiPlayers") === 2) 
+																					|| (playerCount === 2 && form.getValues("aiPlayers") === 1)
+																			) {
 	
-												);
+																				form.setValue("aiPlayers", index + 1);
+
+																			}
+																		}}
+																	/>
+																</FormControl>
+																{index + 1}
+															</FormLabel>
+														</FormItem>
+		
+													);
+
+												}
 
 											} else {
 
