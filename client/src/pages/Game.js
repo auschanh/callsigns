13ec4d0d4e8507bfdad9c5651d65a6134e8dd650
auschanh <auschanh@gmail.com>
2024-03-28 18:46:10 -0400

    guesser checking basics implemented

--- client/src/pages/Game.js
+++ client/src/pages/Game.js
@@ -1,25 +1,44 @@
 import { React, useState } from "react";
 import { Button } from "../components/ui/button";
+import { Input } from "../components/ui/input";
+import { Label } from "../components/ui/label";
 import axios from "axios";
 
 const Game = function (props) {
   const [word, setWord] = useState("");
-  const words = [
-    "Coffee",
-    "Americano",
-    "Latte",
-    "Flat White",
-    "Espresso",
-    "Mocha",
-  ];
+  const [generatedWords, setGeneratedWords] = useState([]);
+  const [guess, setGuess] = useState("");
+
   const generateWord = async () => {
+    // get word from word bank in backend
     const url = "http://localhost:3001/getMysteryWord";
     try {
       const response = await axios.get(url);
-      setWord(response.data);
-      console.log(response.data);
+      const retrievedWord = response.data;
+
+      if (!generatedWords.includes(retrievedWord)) {
+        setWord(retrievedWord);
+        setGeneratedWords([...generatedWords, retrievedWord]);
+        // send to backend
+        // backend emits back to other users to save to their state
+      } else generateWord();
     } catch (error) {
       throw error;
+    }
+  };
+
+  const handleChange = (e) => {
+    setGuess(e.target.value);
+  };
+
+  const checkGuesserWord = () => {
+    const checkGuess = guess.toLowerCase();
+    console.log(checkGuess);
+    const checkWord = word.toLowerCase();
+    console.log(checkWord);
+    if (checkGuess === checkWord) {
+      setGuess(checkGuess);
+      console.log("guessed the word!");
     }
   };
 
@@ -36,7 +55,25 @@ const Game = function (props) {
           Generate a Word
         </Button>
       </div>
-      {word && <div className="mt-4">Generated Word: {word}</div>}
+      {word && (
+        <div className="flex flex-col justify-center items-center gap-2">
+          <div className="mt-4">Generated Word: {word}</div>
+          <br />
+          <Label htmlFor="guess">Guess the word</Label>
+          <Input
+            id="guess"
+            value={guess}
+            placeholder="mystery word"
+            onChange={(e) => {
+              handleChange(e);
+            }}
+          />
+          <Button variant="green" htmlFor="guess" onClick={checkGuesserWord}>
+            Guess
+          </Button>
+          {guess && <div>{guess}</div>}
+        </div>
+      )}
     </div>
   );
 };
