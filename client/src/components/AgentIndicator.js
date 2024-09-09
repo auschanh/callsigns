477ca9a57062b3ac613a656c12ab5d520a43bee3
Justin Wong <justin.c.wong1@gmail.com>
2024-09-08 23:34:29 -0400

    add tooltip to agent icon on slides to make the identity of the guesser very clear

--- client/src/components/AgentIndicator.js
+++ client/src/components/AgentIndicator.js
@@ -1,14 +1,39 @@
 import { ReactComponent as AgentIcon } from "../assets/noun-anonymous-5647770.svg";
+import { Tooltip, TooltipContent, TooltipProvider, TooltipTrigger } from "./ui/tooltip";
 
 function AgentIndicator() {
 
     return (
 
-        <div className="group absolute top-4 left-4 hover:bg-slate-800 rounded-xl transition-all duration-300 cursor-pointer">
+        <TooltipProvider>
 
-            <AgentIcon className="fill-slate-950 aspect-square w-12 group-hover:fill-slate-300 transition-all duration-300" />
+            <Tooltip delayDuration={0}>
 
-        </div>
+                <TooltipTrigger asChild>
+
+                    <div className="group absolute top-4 left-4 hover:bg-slate-800 rounded-xl transition-all duration-300">
+
+                        <AgentIcon className="fill-slate-950 aspect-square w-12 group-hover:fill-slate-300 transition-all duration-300" />
+
+                    </div>
+
+                </TooltipTrigger>
+
+                <TooltipContent className="absolute left-8 w-52">
+
+                    <div>
+
+                        <p className="text-center">You're the Stranded Agent!</p>
+
+                    </div>
+
+                </TooltipContent>
+
+            </Tooltip>
+
+        </TooltipProvider>
+
+        
 
 
     )
